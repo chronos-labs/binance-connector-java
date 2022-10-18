217c38ca1c598f50798c0241f5badc54608c6c62
chronos-labs-sj <shikai@satori.finance>
2022-10-18 16:39:57 -0400

    add snapshotting

--- src/test/java/examples/websocket/BookTicker.java
+++ src/test/java/examples/websocket/BookTicker.java
@@ -24,22 +24,23 @@ public final class BookTicker {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         WebsocketClientImpl client = new WebsocketClientImpl();
-        List<String> symbols = new ArrayList<>(Arrays.asList("BTCUSDT", "ETHUSDT"));
+        List<String> symbols = new ArrayList<>(Arrays.asList("APTUSDT", "APTBUSD"));
 
         BookTicker bookTicker = new BookTicker();
-        bookTicker.shouldStartCapture(symbols);
+        bookTicker.countdown(symbols);
 
         for (String symbol : symbols) {
             bookTicker.captureBookTickerData(client, "bookticker", symbol);
             bookTicker.captureTradeData(client, "trade", symbol);
         }
+        bookTicker.snapshotOrderBook(symbols); // capture after the streams have started
     }
 
-    public void shouldStartCapture(List<String> symbols) throws InterruptedException {
+    public void countdown(List<String> symbols) throws InterruptedException {
         long time = System.currentTimeMillis();
 
         // long sleep
-        while (time < 1664377199000L) { // 10:59:59 EST = Epoch 1664377199000L
+        while (time < 1666141199000L) { // 10:59:59 EST = Epoch 1664377199000L
             System.out.println(time);
             Thread.sleep(500);
             time = System.currentTimeMillis();
@@ -48,7 +49,7 @@ public final class BookTicker {
         System.out.println("Entering countdown");
 
         // short sleep
-        while (time < 1664377200000L || !isListed(symbols)) { // 11:00:00 EST = Epoch 1664377200000L
+        while (time < 1666141200000L || !isListed(symbols)) { // 11:00:00 EST = Epoch 1664377200000L
             Thread.sleep(1);
             time = System.currentTimeMillis();
         }
@@ -72,6 +73,24 @@ public final class BookTicker {
         parameters.clear();
 
         return true;
+    }
+
+    public void snapshotOrderBook(List<String> symbols) throws IOException {
+        String filePrefix = "orderBookSnapshot";
+        LinkedHashMap<String, Object> parameters = new LinkedHashMap<>();
+        SpotClientImpl client = new SpotClientImpl(PrivateConfig.API_KEY, PrivateConfig.SECRET_KEY);
+
+
+        for (String symbol : symbols) {
+            String filename = String.format("%s-%s.txt", symbol, filePrefix);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true));
+
+            System.out.println(String.format("Snapshotting order book for %s", symbol));
+            parameters.put("symbol", symbol);
+            String result = client.createMarket().depth(parameters);
+            writer.append(result);
+            writer.close();
+        }
     }
 
     public void captureBookTickerData(WebsocketClientImpl client,
@@ -110,7 +129,7 @@ public final class BookTicker {
                 writer.append(data + ", localTimestamp=" + System.currentTimeMillis() + "\n");
 
                 long bytes = Files.size(path);
-                if (bytes > 1000000) { // 1mb
+                if (bytes > 20000000) { // 20mb
                     writer.close();
                     fileNumSuffix++;
                     filename = String.format("%s-%s-output-%d.txt", symbol, filePrefix, fileNumSuffix);
